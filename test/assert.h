d4ab72ebfd58a2985f657168ca6d0b7b5d0f1fd6
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-05-09 09:10:44 +0530

    modified assert.h and test.h moved #defines

--- test/assert.h
+++ test/assert.h
@@ -3,15 +3,6 @@
 
 #define FOR_EACH(startAt, endAt) for (int i = startAt; i < endAt; i++)
 
-// ✔ = \u2714  HEAVY CHECK MARK
-// ✘ = \u2718  HEAVY BALLOT X
-
-#define RESET "\033[0m"
-#define GREEN "\033[32m"
-#define RED "\033[31m"
-#define PASS GREEN "\u2714 " RESET
-#define FAIL RED "\u2718 " RESET
-
 Bool assert_integer(int actual, int expected);
 Bool assert_Array(Array *actual, Array *expected);
 
--- test/test.h
+++ test/test.h
@@ -3,6 +3,15 @@
 
 #define PRINT_STRING(string) printf("%s\n", string)
 
+// ✔ = \u2714  HEAVY CHECK MARK
+// ✘ = \u2718  HEAVY BALLOT X
+
+#define RESET "\033[0m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define PASS GREEN "\u2714 " RESET
+#define FAIL RED "\u2718 " RESET
+
 int PASSING_TESTS = 0;
 int FAILING_TESTS = 0;
 
