20ae9d483cdf956bf7552731436a360582dd806a
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-05-09 23:19:10 +0530

    Implemented filter_void feature

--- array_void.c
+++ array_void.c
@@ -28,3 +28,24 @@ ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper)
   }
   return new_array;
 }
+
+ArrayVoid_ptr filter_void(ArrayVoid_ptr src, PredicateVoid predicate)
+{
+  Object filtered_values[src->length];
+  int filters_count = 0;
+  FOR_EACH(0, src->length)
+  {
+    Bool result = (*predicate)(src->array[i]);
+    if (result)
+    {
+      filtered_values[filters_count] = src->array[i];
+      filters_count++;
+    }
+  }
+  ArrayVoid_ptr new_array = create_ArrayVoid_from(filters_count);
+  FOR_EACH(0, new_array->length)
+  {
+    new_array->array[i] = filtered_values[i];
+  }
+  return new_array;
+}
--- numbers.c
+++ numbers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "string.h"
 #include "array.h"
 #include "array_void.h"
 
@@ -22,8 +23,8 @@ int sum(int context, int value)
   return context + value;
 }
 
-void display_integer(void *number);
-void display_integer(void *number)
+void display_integer(Object number);
+void display_integer(Object number)
 {
   printf("%d ", *(int *)number);
 }
@@ -33,6 +34,11 @@ Object square_of_num_void(Object value)
   int *result = malloc(sizeof(int));
   *result = (*(int *)value) * (*(int *)value);
   return result;
+}
+
+Bool is_even_void(Object value)
+{
+  return (*(int *)value) % 2 == 0;
 }
 
 int main(void);
@@ -46,19 +52,19 @@ int main(void)
   display_Array(numbers);
 
   PRINT_STRING("------------ Normal Map Filter Reduce ------------");
-  
+
   newArray = map(numbers, &square_of_num);
   PRINT_STRING("Squares are :");
   display_Array(newArray);
-  
+
   newArray = filter(numbers, &is_even);
   PRINT_STRING("Even Numbers are :");
   display_Array(newArray);
-  
+
   int total = reduce(numbers, 0, &sum);
   PRINT_STRING("Sum of Numbers is :");
   printf("%d\n", total);
-  
+
   PRINT_STRING("------------ Void Map Filter Reduce ------------");
 
   ArrayVoid_ptr numbers_void, new_void_array;
@@ -67,8 +73,12 @@ int main(void)
   {
     numbers_void->array[i] = &values[i];
   }
-  
+
   new_void_array = map_void(numbers_void, square_of_num_void);
   PRINT_STRING("Squares are :");
+  display_ArrayVoid(new_void_array, &display_integer);
+
+  new_void_array = filter_void(numbers_void, is_even_void);
+  PRINT_STRING("Even Numbers are :");
   display_ArrayVoid(new_void_array, &display_integer);
 }
